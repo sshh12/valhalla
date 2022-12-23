d3fbb6b1a3b6356ff8aa210fd8eeaaae9c60caa1
Shrivu Shankar <shrivu1122@gmail.com>
2022-12-23 13:03:10 -0800

    Update run-gateway.sh

--- network/run-gateway.sh
+++ network/run-gateway.sh
@@ -24,12 +24,15 @@ sudo iptables -t nat -A REDSOCKS -d 198.18.0.0/15 -j RETURN
 sudo iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
 sudo iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
 
-
-# redirect
+# socks redirect
 sudo iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
-
 sudo iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
-
 sudo iptables -t nat -A PREROUTING -p tcp -m iprange --src-range 10.0.0.2-10.0.0.100 -j REDSOCKS
+
+# vpn redirect
+sudo iptables -t nat -A POSTROUTING -m iprange --src-range 10.0.0.102-10.0.0.200 -o tun0 -j MASQUERADE
+sudo iptables -A FORWARD -o tun0 -j ACCEPT
+sudo iptables -A FORWARD -i tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -i tun0 -j ACCEPT
 
 sudo /usr/bin/glider -listen redir://:12345 -forward socks5://127.0.0.1:4080 -dns=:53 -dnsserver=8.8.8.8:53 -verbose
