fb8eab096c1344ef0ef76ee8200f9768fceac157
Shrivu Shankar <shrivu1122@gmail.com>
2023-01-01 10:42:24 -0800

    Update run-vpn.sh

--- network/run-vpn.sh
+++ network/run-vpn.sh
@@ -1,4 +1,9 @@
  #!/bin/sh
  # auth-user-pass login.conf
- sudo openvpn --config us5066.nordvpn.com.tcp443.ovpn
+ 
+ # update system time for crypto
+ sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-10)"
+ 
+ # run vpn
+ sudo openvpn --config us5066.nordvpn.com.tcp443.ovpn --tls-exit
  
