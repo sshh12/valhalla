6c85b480c72275b8b9fbdec517f708d601796fef
Shrivu Shankar <shrivu1122@gmail.com>
2023-01-01 11:16:08 -0800

    Update run-vpn.sh

--- network/run-vpn.sh
+++ network/run-vpn.sh
@@ -5,5 +5,5 @@
  sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-10)"
  
  # run vpn
- sudo openvpn --config us5066.nordvpn.com.tcp443.ovpn --tls-exit
+ sudo openvpn --config us9436.nordvpn.com.tcp443.ovpn --tls-exit --socks-proxy 127.0.0.1 4080
  
